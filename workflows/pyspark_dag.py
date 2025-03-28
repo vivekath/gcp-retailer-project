5452e8a5471f6d718248bf2eed960a4792ae1354
shaiksaidhul <157446745+shaiksaidhul@users.noreply.github.com>
2025-03-28 16:12:18 +0530

    updated

--- workflows/pyspark_dag.py
+++ workflows/pyspark_dag.py
@@ -13,7 +13,7 @@ from airflow.providers.google.cloud.operators.dataproc import (
 PROJECT_ID = "avd-databricks-demo"
 REGION = "us-east1"
 CLUSTER_NAME = "my-demo-cluster"
-COMPOSER_BUCKET = "us-central1-demo-instance-968eb987-bucket"
+COMPOSER_BUCKET = "us-central1-demo-instance-9e2e6451-bucket"
 
 GCS_JOB_FILE_1 = f"gs://{COMPOSER_BUCKET}/data/INGESTION/retailerMysqlToLanding.py"
 PYSPARK_JOB_1 = {
